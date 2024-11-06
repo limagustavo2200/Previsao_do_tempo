24e4a5a6588c13bfd96b0da187438d09a09bdefd
Gustavo Lima de Carvalho <109549872+limagustavo2200@users.noreply.github.com>
2024-11-06 15:02:22 -0300

    feat: animation title;  implementer try catch

--- previsao/src/App.jsx
+++ previsao/src/App.jsx
@@ -5,26 +5,29 @@ import WeatherInformations from './components/WeatherInformations'
 import WeatherInformations5Days from './components/WeatherInformations5Days/WeatherInformation5Days'
 
 function App() {
-
-  const [weather, setWeather] = useState()
-  const [weather5Days, setWeather5Days] = useState()
+  const [weather, setWeather] = useState(null)
+  const [weather5Days, setWeather5Days] = useState(null)
+  const [status, setStatus] = useState(0)
+  const [error, setError] = useState(null)
   const inputRef = useRef()
 
   const searchCity = async () => {
     const city = inputRef.current.value
-
-    const key = '76414720d10a4b4df163886ad8278919'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
-
-    const url5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    const keyApi = '76414720d10a4b4df163886ad8278919'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}&lang=pt_br&units=metric`
+    const url5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${keyApi}&lang=pt_br&units=metric`
 
     try {
       const apiInfo = await axios.get(url);
       const apiInfo5Days = await axios.get(url5Days);
 
       setWeather(apiInfo.data);
-      setWeather5Days(apiInfo5Days.data); 
+      setWeather5Days(apiInfo5Days.data);
+      setStatus(preStatus => preStatus + 1);
+      setError(null); 
+
     } catch (error) {
+      setError("Nome não encontrado"); 
       console.error("Erro ao buscar dados da API:", error);
     }
   }
@@ -32,10 +35,24 @@ function App() {
   return (
     <div className='container'>
       <h1>Previsão do Tempo</h1>
-      <input ref={inputRef} type="text" placeholder='Digite o nome da cidade' />
-      <button onClick={searchCity}>Buscar</button>
-      {weather ? <WeatherInformations weather={weather} /> : null}
-      {weather5Days && <WeatherInformations5Days weather5Days={weather5Days} />}
+      <div className='inputWithButton'>
+        <input ref={inputRef} type="text" placeholder='Digite o nome da cidade' />
+        <button onClick={searchCity}>Buscar</button>
+      </div>
+
+      {error && <p>{error}</p>} 
+
+      {weather && (
+        <div key={`weather-${status}`} className="slide-in-left">
+          <WeatherInformations weather={weather} />
+        </div>
+      )}
+      {weather5Days && (
+        <div key={`weather5Days-${status}`} className="slide-in-right">
+          <WeatherInformations5Days weather5Days={weather5Days} />
+        </div>
+      )}
+
     </div>
   )
 }
